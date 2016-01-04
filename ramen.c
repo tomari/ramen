b3f7f0e48e8274b2e111b037868fb37bcfabd7ed
Hisanobu Tomari <htomari@myopera.com>
2016-01-04 10:04:05 +0900

    Cleanup preference handling

--- ramen.c
+++ ramen.c
@@ -5,6 +5,14 @@
 #include "ramenrsc.h"
 #include "ramenprefs.h"
 
+typedef struct {
+	Int32 BombTime;
+	Int32 RamenTime;
+	Boolean TimerStarted;
+	UInt32 Presets[5];
+	UInt8 AlarmVolume;
+} ramenPreferenceType;
+
 static void PaintNumbers(Int32 ramen);
 static void setRamenTo(Int32 ramen);
 static void SetAlarm(void);
@@ -12,19 +20,26 @@ static void DisplayAlarm(void);
 static void refreshDisplay(void);
 static Boolean isTheDeviceSupported(void);
 static void refreshButtons(void);
-static void RamenLoadPreferences(Int32 *LocalBombTime, Int32 *LocalRamenTime, Boolean *LocalTimerStarted, UInt32 *LocalButtonNumbers, UInt8 *LocalAlarmVolume);
+static void RamenLoadPreferences(ramenPreferenceType *);
 static void RamenSavePreferences(void);
 static void RamenPlaySound(void);
 
+/* Constants */
+const Int32 MAXTIME=99;
+const Int32 MINTIME=1;
+static const UInt32 MYCREATORID='Ramn';
+
 /* var */
-Int32 RamenTime=DEFTIME;
-Boolean TimerStarted = false;
-UInt32 BombTime = 0;
+ramenPreferenceType rp={
+	.BombTime = 0,
+	.RamenTime = 3,
+	.TimerStarted = false,
+	.Presets = {1, 3, 5, 10, 60},
+	.AlarmVolume = 3
+};
 UInt32 lastTimeRefreshed = 0;
-UInt32 ButtonNumbers[5] = {1, 3, 5, 10, 60};
 Char ButtonLabels[5][3];
 Char NumLabel[3];
-UInt8 AlarmVolume = 3;
 
 /* implementation */
 static Boolean MainFormHandleEvent(EventPtr eventP) {
@@ -35,16 +50,16 @@ static Boolean MainFormHandleEvent(EventPtr eventP) {
 			MenuEraseStatus(0);
 			FrmAlert(AboutAlert);
 		} else if (itemID == MenuPrefs) {
-			DoPrefs(ButtonNumbers, &AlarmVolume);
+			DoPrefs(rp.Presets, &rp.AlarmVolume);
 			refreshButtons();
 			RamenSavePreferences();
 		}
 	} else if(eType == ctlRepeatEvent) {
 		const UInt16 controlID = eventP->data.ctlRepeat.controlID;
 		if(controlID == REPEATUP) {
-			setRamenTo(RamenTime + 1);
+			setRamenTo(rp.RamenTime + 1);
 		} else if(controlID == REPEATDOWN) {
-			setRamenTo(RamenTime - 1);
+			setRamenTo(rp.RamenTime - 1);
 		}
 		return false; /* leave unhandled so buttons can repeat */
 	} else if(eType == ctlSelectEvent) {
@@ -56,30 +71,30 @@ static Boolean MainFormHandleEvent(EventPtr eventP) {
 			  controlID == PRESET3 ||
 			  controlID == PRESET4 ||
 			  controlID == PRESET5) {
-			setRamenTo(ButtonNumbers[eventP->data.ctlSelect.controlID - PRESET1]);
+			setRamenTo(rp.Presets[eventP->data.ctlSelect.controlID - PRESET1]);
 		}
 	} else if(eType == keyDownEvent) {
 		const WChar thekey =  eventP->data.keyDown.chr;
 		if ((L'0' <= thekey) && (thekey <= L'9')) {
 			const Int32 digit=thekey - L'0';
-			if(RamenTime > 9) {
+			if(rp.RamenTime > 9) {
 				setRamenTo(digit);
 			} else {
-				setRamenTo(RamenTime*10+digit);
+				setRamenTo(rp.RamenTime*10+digit);
 			}
 		} else if(thekey == vchrPageUp ||
 			  thekey == vchrJogUp) {
-			setRamenTo(RamenTime+1);
+			setRamenTo(rp.RamenTime+1);
 		} else if(thekey == vchrPageDown ||
 			  thekey == vchrJogDown) {
-			setRamenTo(RamenTime-1);
+			setRamenTo(rp.RamenTime-1);
 		} else if(NavSelectPressed(eventP) || 
 			  thekey == vchrJogPush) {
 			SetAlarm();
 		} else if(thekey == vchrJogPushedUp) {
-			setRamenTo(RamenTime + 10);
+			setRamenTo(rp.RamenTime + 10);
 		} else if(thekey == vchrJogPushedDown) {
-			setRamenTo(RamenTime - 10);
+			setRamenTo(rp.RamenTime - 10);
 		} else {
 			return false;
 		}
@@ -103,7 +118,7 @@ static Boolean AppHandleEvent(EventPtr eventP) {
 	} else if (eventP->eType == frmOpenEvent) { /* Load the form resource. */
 		const FormPtr frmP = FrmGetActiveForm();
 		FrmDrawForm(frmP);
-		PaintNumbers(RamenTime);
+		PaintNumbers(rp.RamenTime);
 		refreshDisplay();
 	} else if (eventP->eType == appStopEvent) { /* Unload the form resource. */
 		const FormPtr frmP = FrmGetActiveForm();
@@ -138,11 +153,11 @@ static void setRamenTo(Int32 ramen) {
 		scrollableUp=false;
 		scrollableDown=true;
 	}
-	RamenTime = ramen;
-	BombTime = 0; /*why did i add this code?*/
-	TimerStarted=true; /* Tell SetAlarm() to cancel any previously registered timer */
+	rp.RamenTime = ramen;
+	rp.BombTime = 0; /*why did i add this code?*/
+	rp.TimerStarted=true; /* Tell SetAlarm() to cancel any previously registered timer */
 	SetAlarm();
-	PaintNumbers(RamenTime);
+	PaintNumbers(rp.RamenTime);
 	if(CtlEnabled(UpButtonP) != scrollableUp) {
 		CtlSetEnabled(UpButtonP, scrollableUp);
 		if(scrollableUp == false) {
@@ -184,23 +199,23 @@ static void SetAlarm() {
 	UInt16 cardNo;
 	LocalID dbID;
 	SysCurAppDatabase(&cardNo, &dbID);
-	if (TimerStarted == false) {
-		if (!BombTime) {
-			BombTime = TimGetSeconds() + RamenTime * 60;
+	if (!rp.TimerStarted) {
+		if (!rp.BombTime) {
+			rp.BombTime = TimGetSeconds() + rp.RamenTime * 60;
 		}
 		{
-			const Err errID=AlmSetAlarm(cardNo, dbID, 0, BombTime, true);
+			const Err errID=AlmSetAlarm(cardNo, dbID, 0, rp.BombTime, true);
 			if(errID == almErrMemory) {
 				FrmAlert(OUTOFMEMORYALERT);
 			} else if(errID == almErrFull) {
 				FrmAlert(TABLEFULLALERT);
 			} else {
-				TimerStarted = true;
+				rp.TimerStarted = true;
 			}
 		}
 	} else { /* cancel timer */
 		AlmSetAlarm(cardNo, dbID, 0, 0, true);
-		TimerStarted = false;
+		rp.TimerStarted = false;
 	}
 }
 
@@ -215,11 +230,11 @@ static void DisplayAlarm() {
 }
 
 static void refreshDisplay() {
-	if (TimerStarted == true) {
+	if (rp.TimerStarted) {
 		const Int32 now = TimGetSeconds();
 		const Int32 interval = now - lastTimeRefreshed;
 		if (interval) {
-			const Int32 timeremaining = BombTime - now;
+			const Int32 timeremaining = rp.BombTime - now;
 			Int32 dispTime;
 			if (timeremaining > 60) {
 				if(now&0x1) {
@@ -228,10 +243,10 @@ static void refreshDisplay() {
 					dispTime = timeremaining / 60;
 				}
 			} else if (timeremaining <= 0) {
-				dispTime=RamenTime;
-				TimerStarted = false;
+				dispTime=rp.RamenTime;
+				rp.TimerStarted = false;
 				lastTimeRefreshed = 0;
-				BombTime = 0;
+				rp.BombTime = 0;
 			} else {
 				dispTime = timeremaining;
 			}
@@ -251,7 +266,7 @@ static void refreshButtons() {
 	const FormPtr frmP = FrmGetActiveForm();
 	unsigned int i=4; do {
 		const ControlPtr theButton=FrmGetObjectPtr(frmP, FrmGetObjectIndex(frmP,PRESET1+i));
-		StrIToA(ButtonLabels[i], ButtonNumbers[i]);
+		StrIToA(ButtonLabels[i], rp.Presets[i]);
 		CtlSetLabel(theButton, ButtonLabels[i]);
 	} while(i--);
 }
@@ -259,59 +274,36 @@ static void refreshButtons() {
 /*
 	Global-Free Preference Loader for AlarmVolume implementation
 */
-static void RamenLoadPreferences(LocalBombTime, LocalRamenTime, LocalTimerStarted, LocalButtonNumbers, LocalAlarmVolume)
-	Int32 *LocalBombTime;
-	Int32 *LocalRamenTime;
-	Boolean *LocalTimerStarted;
-	UInt32 *LocalButtonNumbers;
-	UInt8 *LocalAlarmVolume;
-{
-	ramenPreferenceType ramenPrefs;
-	UInt16 prefsSize = sizeof(ramenPreferenceType);
-	UInt8 counter;
-	
-	if (PrefGetAppPreferences(MYCREATORID, 0, &ramenPrefs, &prefsSize, false) != noPreferenceFound) {
-		if (prefsSize == sizeof(ramenPreferenceType)) {
-			*LocalBombTime = ramenPrefs.BombTime;
-			*LocalRamenTime = ramenPrefs.RamenTime;
-			for (counter=0; counter<5; counter++) {
-				*(LocalButtonNumbers + counter) = ramenPrefs.Presets[counter];
+static void RamenLoadPreferences(ramenPreferenceType *pref_addr) {
+	UInt16 prefsSize=0U;
+	if(PrefGetAppPreferences(MYCREATORID, 0, NULL, &prefsSize, false) != noPreferenceFound) {
+		if(prefsSize==sizeof(ramenPreferenceType)) {
+			PrefGetAppPreferences(MYCREATORID,0,pref_addr,&prefsSize,false);
+			if(TimGetSeconds()>pref_addr->BombTime) {
+				pref_addr->TimerStarted=false;
 			}
-			*LocalAlarmVolume = ramenPrefs.AlarmVolume;
-			if(!(TimGetSeconds() > *LocalBombTime)) {
-				*LocalTimerStarted = ramenPrefs.TimerStarted;
-			}
-			if(*LocalTimerStarted == false) {
-				*LocalBombTime = 0;
+			if(!pref_addr->TimerStarted) {
+				pref_addr->BombTime = 0;
 			}
 		}
 	}
 }
 
 static void RamenSavePreferences() {
-	ramenPreferenceType rPT1;
-	
-	rPT1.BombTime = BombTime;
-	rPT1.RamenTime = RamenTime;
-	rPT1.TimerStarted = TimerStarted;
-	rPT1.AlarmVolume = AlarmVolume;
-	{
-		unsigned int i=4; do {
-			rPT1.Presets[i] = ButtonNumbers[i];
-		} while(i--);
-	}
-	PrefSetAppPreferences(MYCREATORID, 0, 1, &rPT1, sizeof(rPT1), false);
+	PrefSetAppPreferences(MYCREATORID, 0, 1, &rp, sizeof(rp), false);
 }
 
 /* Function to play alarm sound; NO GLOBAL VARIABLE CAN BE USED!!! */
 static void RamenPlaySound() {
-	Int32 junk[5];
-	Boolean BoolJunk = false;
-	UInt8 VolumeOfAlarm;
-	SndCommandType snd;
-	RamenLoadPreferences(junk, junk, &BoolJunk, junk, &VolumeOfAlarm);
-	if(VolumeOfAlarm<3) {
-		snd.param3 = 16 + VolumeOfAlarm * 24;
+	ramenPreferenceType lp;
+	SndCommandType snd = {
+		.param1=1760,
+		.param2=230,
+		.cmd = sndCmdFreqDurationAmp
+	};
+	RamenLoadPreferences(&lp);
+	if(lp.AlarmVolume<3) {
+		snd.param3 = 16 + lp.AlarmVolume * 24;
 	} else {
 		const UInt16 prefver=PrefGetPreference(prefVersion);
 		if(prefver<3) {
@@ -321,9 +313,6 @@ static void RamenPlaySound() {
 			snd.param3=PrefGetPreference(prefAlarmSoundVolume);
 		}
 	}
-	snd.param1=1760;
-	snd.param2=230;
-	snd.cmd = sndCmdFreqDurationAmp;
 	{
 		const UInt16 ticks_per_sec = SysTicksPerSecond();
 		unsigned int counter1=2;
@@ -346,7 +335,7 @@ UInt32 PilotMain(UInt16 cmd, MemPtr cmdPBP, UInt16 launchFlags) {
 			return 1;
 		}
 		/* Load my preferences */
-		RamenLoadPreferences(&BombTime, &RamenTime, &TimerStarted, ButtonNumbers, &AlarmVolume);
+		RamenLoadPreferences(&rp);
 		
 		FrmGotoForm(MainForm);
 		do {
--- ramen.h
+++ ramen.h
@@ -1,14 +1,2 @@
-typedef struct {
-	Int32 BombTime;
-	Int32 RamenTime;
-	Boolean TimerStarted;
-	UInt32 Presets[5];
-	UInt8 AlarmVolume;
-} ramenPreferenceType;
-
-#define MAXTIME 99
-#define MINTIME 1
-#define DEFTIME 3
-#define MYCREATORID 'Ramn'
-#define DISPLAYX 60
-#define DISPLAYY 44
+extern const Int32 MAXTIME;
+extern const Int32 MINTIME;
